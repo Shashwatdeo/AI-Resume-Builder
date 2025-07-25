1a59ff528724c23caeac196075062882b49ca3bb
Shashwat Deo <shashwatdeo143@gmail.com>
2025-07-25 14:38:20 +0530

    first commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,9 +14,12 @@ import path from 'path';
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true,              
-}));
+    origin: [
+      "http://localhost:5173", // for local dev
+      "https://ai-resume-builder-lbyx.vercel.app/" // for production, replace with your actual Vercel URL
+    ],
+    credentials: true,
+  }));
 
 app.use(express.json({ limit: '5mb' })); // or even higher if needed
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
